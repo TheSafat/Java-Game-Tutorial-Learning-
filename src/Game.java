d13720ec0615befa67b90912f9342b4d450c4f59
Safat Hossain <thesafat@gmail.com>
2022-03-05 10:40:26 +0600

    player image has been added. finished upto tutorial 5

--- src/Game.java
+++ src/Game.java
@@ -5,19 +5,24 @@ import java.awt.event.ActionListener;
 
 public class Game extends JPanel implements ActionListener {
     Timer timer;
+    Player p;
     public Game(){
         setFocusable(true);
         timer = new Timer(17, this);
         timer.start();
+        p = new Player(100, 100);
     }
 
     public void paint(Graphics g){
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
+        p.draw(g2d);
+        //g2d.setBackground(Color.BLACK);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        System.out.println("action performed");
+        //System.out.println("action performed");
+        repaint();
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,32 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+
+public class Player {
+    int x;
+    int y;
+
+
+    public Player(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(){
+
+    }
+
+    public void draw(Graphics2D g2d){
+        //System.out.println("Hello from draw");
+        g2d.drawImage(getPlayerImage(), x, y, null);
+
+        //g2d.fillRect(40, 50, 60, 80);
+
+    }
+
+    Image getPlayerImage(){
+        ImageIcon i = new ImageIcon(getClass().getResource("/player.png"));
+
+        return i.getImage();
+    }
+}
